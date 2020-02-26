e57d209be943a0c6d2c033432e546fe76feb9fd4
linted <linted@users.noreply.github.com>
2020-02-26 17:52:23 -0500

    fixed output and typos

--- code/run_tests.sh
+++ code/run_tests.sh
@@ -13,19 +13,33 @@ cleanup () {
     exit
 }
 
+flush_cache () {
+    sudo sync
+    echo 1 | sudo tee /proc/sys/vm/drop_caches 1>/dev/null
+}
+
 light_usage_tests () {
+    echo "**************************************"
+    echo "***" Light Usage Test with $1
+    echo "**************************************"
+    flush_cache
     LD_PRELOAD=./$1 ./light_usage_test --benchmark_min_time=$2 --benchmark_repetitions=$3 --benchmark_out=light_$1_$2s_$3.json --benchmark_out_format=json --benchmark_display_aggregates_only=true
 }
 
 sporatic_usage_tests () {
+    echo "**************************************"
+    echo "***" Sporatic Usage Test with $1
+    echo "**************************************"
+    flush_cache
     LD_PRELOAD=./$1 ./sporatic_usage_test --benchmark_min_time=$2 --benchmark_repetitions=$3 --benchmark_out=sporatic_$1_$2s_$3.json --benchmark_out_format=json --benchmark_display_aggregates_only=true
 }
 
 ALLOCATION_LIBRARIES="libhoard.so  libjemalloc.so.2  libptmalloc3.so  libtcmalloc_minimal.so  libtcmalloc.so libc.so.6"
 for allocator in $ALLOCATION_LIBRARIES; do
+    echo Starting work on $allocator
     chmod +x $allocator
-    light_usage_tests $allocator 5 10
-    sporatic_usage_tests $allocator 5 10
+    light_usage_tests $allocator 1 20
+    sporatic_usage_tests $allocator 1 20
 done
 
 kill -9 $ncat_pid
