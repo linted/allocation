8c54ebfb4b61eeb5895a423089c8202ee9909e36
linted <linted@users.noreply.github.com>
2020-02-26 15:43:34 -0500

    updated test options to only run 10 iterations

--- code/run_tests.sh
+++ code/run_tests.sh
@@ -14,18 +14,18 @@ cleanup () {
 }
 
 light_usage_tests () {
-    LD_PRELOAD=$1 ./light_usage_test --benchmark_min_time=$2 --benchmark_repetitions=$3 --benchmark_out=light_$1_$2s_$3.json --benchmark_out_format=json --benchmark_display_aggregates_only=true
+    LD_PRELOAD=./$1 ./light_usage_test --benchmark_min_time=$2 --benchmark_repetitions=$3 --benchmark_out=light_$1_$2s_$3.json --benchmark_out_format=json --benchmark_display_aggregates_only=true
 }
 
 sporatic_usage_tests () {
-    LD_PRELOAD=$1 ./sporatic_usage_test --benchmark_min_time=$2 --benchmark_repetitions=$3 --benchmark_out=sporatic_$1_$2s_$3.json --benchmark_out_format=json --benchmark_display_aggregates_only=true
+    LD_PRELOAD=./$1 ./sporatic_usage_test --benchmark_min_time=$2 --benchmark_repetitions=$3 --benchmark_out=sporatic_$1_$2s_$3.json --benchmark_out_format=json --benchmark_display_aggregates_only=true
 }
 
 ALLOCATION_LIBRARIES="libhoard.so  libjemalloc.so.2  libptmalloc3.so  libtcmalloc_minimal.so  libtcmalloc.so libc.so.6"
 for allocator in $ALLOCATION_LIBRARIES; do
     chmod +x $allocator
-    light_usage_tests $allocator 5 100
-    sporatic_usage_tests $allocator 5 100
+    light_usage_tests $allocator 5 10
+    sporatic_usage_tests $allocator 5 10
 done
 
 kill -9 $ncat_pid
