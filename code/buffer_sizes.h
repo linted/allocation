4e438b087b2ddb367736ad86c035f90d2e65cab8
linted <linted@users.noreply.github.com>
2020-02-26 21:21:08 -0500

    updated buffer sizes to be a better distribution, and added some unused sizes for future development

--- code/buffer_sizes.h
+++ code/buffer_sizes.h
@@ -6,10 +6,12 @@ extern "C"
 { 
 #endif
 
-#define HUGE_BUFFER_SIZE 65445
-#define LARGE_BUFFER_SIZE 1024
-#define MEDIUM_BUFFER_SIZE 200
-#define SMALL_BUFFER_SIZE 16
+#define GIANT_BUFFER_SIZE 65536
+#define HUGE_BUFFER_SIZE 32768
+#define LARGE_BUFFER_SIZE 4096
+#define MEDIUM_BUFFER_SIZE 256
+#define SMALL_BUFFER_SIZE 32
+#define MINI_BUFFER_SIZE 16
 
 #ifdef __cplusplus
 }
