9ce2929b4287a2b5deff1a0408995a37acf10dee
linted <linted@users.noreply.github.com>
2020-02-26 21:13:53 -0500

    added a common buffer size headder to match sizes across the different test files

---
+++ code/buffer_sizes.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef _BUFFER_SIZES_H
+#define _BUFFER_SIZES_H
+#ifdef __cplusplus
+extern "C"
+{ 
+#endif
+
+#define HUGE_BUFFER_SIZE 65445
+#define LARGE_BUFFER_SIZE 1024
+#define MEDIUM_BUFFER_SIZE 200
+#define SMALL_BUFFER_SIZE 16
+
+#ifdef __cplusplus
+}
+#endif
+#endif
