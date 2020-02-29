dafd9c9c205cd2c235b385311cb40170bf46371e
linted <linted@users.noreply.github.com>
2020-02-28 20:41:00 -0500

    annotated the buffer sizes chosen

--- code/buffer_sizes.h
+++ code/buffer_sizes.h
@@ -6,12 +6,12 @@ extern "C"
 { 
 #endif
 
-#define GIANT_BUFFER_SIZE 65536
-#define HUGE_BUFFER_SIZE 32768
-#define LARGE_BUFFER_SIZE 4096
-#define MEDIUM_BUFFER_SIZE 256
-#define SMALL_BUFFER_SIZE 32
-#define MINI_BUFFER_SIZE 16
+#define GIANT_BUFFER_SIZE 65536     // 2^16
+#define HUGE_BUFFER_SIZE 32768      // 2^15
+#define LARGE_BUFFER_SIZE 4096      // 2^12
+#define MEDIUM_BUFFER_SIZE 256      // 2^8
+#define SMALL_BUFFER_SIZE 32        // 2^5
+#define MINI_BUFFER_SIZE 16         // 2^4
 
 #ifdef __cplusplus
 }
