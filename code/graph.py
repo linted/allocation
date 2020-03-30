5a317815f794f2c6057d8a491b474a18411be0c0
linted <linted@users.noreply.github.com>
2020-03-30 17:11:38 -0400

    updated graph.py to use plotly

--- code/graph.py
+++ code/graph.py
@@ -3,6 +3,7 @@ import pandas as pd
 # import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from plotly.io import orca
 import json
 import argparse
 import re
@@ -14,15 +15,16 @@ def main():
     parser.add_argument("results_file", help="google benchmark results json output")
     parser.add_argument('-g',"--groups", help="list of args", nargs='+', default=['.*'])
     parser.add_argument('-o',"--output", help="output file for the graph")
+    parser.add_argument('-s',"--server", help="orca server url", default="http://localhost:9091")
     args = parser.parse_args()
 
     # print(dir(args))
     with open(args.results_file, 'r') as fin:
         results = json.load(fin)
 
-    prog_name = results['context']['executable']
-    debug = results['context']['library_build_type'] != 'release'
-    scaling_enabled = results['context']['cpu_scaling_enabled']
+    # prog_name = results['context']['executable']
+    # debug = results['context']['library_build_type'] != 'release'
+    # scaling_enabled = results['context']['cpu_scaling_enabled']
     yaxis = 'bytes_per_second' if results['benchmarks'][0].get("bytes_per_second", False) else 'cpu_time'
 
     raw_data = pd.DataFrame(results['benchmarks']).dropna(subset=['aggregate_name'])
@@ -35,8 +37,12 @@ def main():
         # fig.show()
         figure.append_trace(go.Bar(y=data.run_name, x=data[yaxis], orientation='h', name=args.groups[group]), 1+(group//2), 1+(group%2))
         
-    figure.show()
-    
+    if args.output == None:
+        figure.show()
+    else:
+        orca.config.server_url = args.server
+        figure.write_image(args.output)
+            
 
 if __name__ == "__main__":
     main()
