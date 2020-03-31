95f5ff3329d3200ec6a7d079973d5fa9c7f49068
linted <linted@users.noreply.github.com>
2020-03-31 16:10:40 -0400

    I hate myself for makingthis code, but I also love it

--- code/graph.py
+++ code/graph.py
@@ -6,9 +6,8 @@ from plotly.subplots import make_subplots
 from plotly.io import orca
 import json
 import argparse
-import re
 from math import ceil
-from os import path
+from os.path import basename
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,21 +21,41 @@ def main():
     with open(args.results_file, 'r') as fin:
         results = json.load(fin)
 
-    # prog_name = results['context']['executable']
+    plot_title = ' '.join(x[0].capitalize() + x[1:] for x in results['context']['executable'].lstrip('./').split("_")) + " using " + basename(args.results_file).split("_")[1]
     # debug = results['context']['library_build_type'] != 'release'
     # scaling_enabled = results['context']['cpu_scaling_enabled']
     yaxis = 'bytes_per_second' if results['benchmarks'][0].get("bytes_per_second", False) else 'cpu_time'
 
     raw_data = pd.DataFrame(results['benchmarks']).dropna(subset=['aggregate_name'])
-
+    raw_data = raw_data['mean' == raw_data.aggregate_name]
     figure = make_subplots(rows=ceil(len(args.groups)/2), cols=2 if len(args.groups) > 1 else 1)
 
     for group in range(len(args.groups)):
         data = raw_data[raw_data['name'].str.contains(args.groups[group])]
         # fig = px.bar(data, y="run_name", x=yaxis, orientation='h')
         # fig.show()
-        figure.append_trace(go.Bar(y=data.run_name, x=data[yaxis], orientation='h', name=args.groups[group]), 1+(group//2), 1+(group%2))
-        
+        figure.append_trace(
+            go.Bar(
+                y=data.run_name.str.extract(r"do_([\w_]*)(?:_{})(?:/threads:(\d))?".format(args.groups[group])).dropna(axis=1).agg(':'.join, axis=1), 
+                x=data[yaxis], 
+                orientation='h', 
+                name=args.groups[group],
+                text=data[yaxis].round(2),
+                textposition='auto',
+            ),
+            1+(group//2), 
+            1+(group%2)
+        )
+    figure.update_layout(
+        title={
+            'text': plot_title,
+            # 'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        }
+    )
+
     if args.output == None:
         figure.show()
     else:
